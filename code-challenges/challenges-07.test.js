ffd57bc17ed6659947b202209473b868b6729f10
scottfalbo <scottfalboart@gmail.com>
2020-10-17 17:47:59 -0700

    world 7 level 6

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -94,7 +94,11 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 ------------------------------------------------------------------------------------------------ */
 
 const evenOdd = (arr) => {
-  // Solution code here...
+  return arr.map(value => {
+    if (isNaN(value)){return 'N/A';}
+    if (value%2 === 0){return 'even';}
+    else{return 'odd';}
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -248,7 +252,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
