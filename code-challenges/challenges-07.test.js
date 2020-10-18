671c45794a6d985bb8d09ada00da9d59651d5437
scottfalbo <scottfalboart@gmail.com>
2020-10-17 17:13:21 -0700

    world 6 level 2

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -34,7 +34,11 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 ------------------------------------------------------------------------------------------------ */
 
 const forLoopTwoToThe = (arr) => {
-  // Solution code here...
+  const returnArray = [];
+  for (let n in arr){
+    returnArray.push(Math.pow(2, arr[n]));
+  }
+  return returnArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -196,7 +200,7 @@ describe('Testing challenge 1', () => {
   })
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(forLoopTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forLoopTwoToThe([0, 4, 5]).length).toStrictEqual(3);
