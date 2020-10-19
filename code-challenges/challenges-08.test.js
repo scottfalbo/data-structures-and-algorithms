a9035f0fc465e04c8130a8fb2ae2c00ccbd3a87e
scottfalbo <scottfalboart@gmail.com>
2020-10-19 01:01:38 -0700

    world 8 level 6

--- code-challenges/challenges-08.test.js
+++ code-challenges/challenges-08.test.js
@@ -130,7 +130,12 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
+  let filter = arr.filter(value => {
+    if (value.baseStat > minBaseStat){return true;}
+  });
+  return filter.map(value => {
+    return value.stat.name;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -292,7 +297,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return the name of the stats that exceed that maximum', () => {
     expect(getStatName(snorlaxData.stats, 50)).toStrictEqual([ 'special-defense', 'special-attack' ]);
     expect(getStatName(snorlaxData.stats, 50).length).toStrictEqual(2);
