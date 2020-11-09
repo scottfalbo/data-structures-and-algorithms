74e308ef3aec797c8c976b5868ea11be46c4c30e
scottfalbo <scottfalboart@gmail.com>
2020-11-09 14:47:49 -0800

    world 13 level 8

--- code-challenges/challenges-13.test.js
+++ code-challenges/challenges-13.test.js
@@ -112,7 +112,11 @@ Write a function named findEvery that takes in an array of strings, along with a
 ------------------------------------------------------------------------------------------------ */
 
 const findEvery = (arr, target) => {
-  // Solution code here...
+  let checker = true;
+  arr.forEach(value => {
+    if(!value.includes(target)){checker = false;}
+  });
+  return checker;
 };
 
 /* -----------------------------------------------------------------------------------------------
@@ -247,7 +251,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should determine whether all the strings contain a given string', () => {
     const words = ['things', 'apple pie (:)', ':)banana pie', 'missing that thing', 'cant:)aloupe is tasty'];
 
