c7973a621a4f119ab408063f6ed83f07f5a18327
scottfalbo <scottfalboart@gmail.com>
2020-11-09 14:39:32 -0800

    world 13 level 6

--- code-challenges/challenges-13.test.js
+++ code-challenges/challenges-13.test.js
@@ -86,7 +86,11 @@ Write a function named allHappy that takes in an array of strings and returns a 
 ------------------------------------------------------------------------------------------------ */
 
 const allHappy = (arr) => {
-  // Solution code here...
+  let checker = true;
+  arr.forEach(value => {
+    if(!value.includes(`:)`)){checker = false;}
+  });
+  return checker;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -222,7 +226,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should correctly assess whether all the strings are happy', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
