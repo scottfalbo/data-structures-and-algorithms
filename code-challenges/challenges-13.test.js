b5f50676f18729d1c4419c3b18450e42fb649ea8
scottfalbo <scottfalboart@gmail.com>
2020-11-09 15:03:48 -0800

    world 13 level 10

--- code-challenges/challenges-13.test.js
+++ code-challenges/challenges-13.test.js
@@ -163,7 +163,11 @@ For example, ['Tuesday', 'Monday', 'Wednesday and Thursday', 'Tuesday 2', 'Thurs
 const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 const sortByDay = (arr) => {
-  // Solution code here...
+  return daysOfWeek.map(day => {
+    return arr.filter(value => {
+      if(value.includes(day)){return true;}
+    });
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -283,7 +287,7 @@ describe('Testing challenge 9', () => {
   });
 });
 
-xdescribe('Testing challenge 10', () => {
+describe('Testing challenge 10', () => {
   test('It should sort events by the day on which they happen', () => {
     const events = ['Dancing on Mondays and Tuesdays', 'Meet the inventors! Monday, August 7', 'in the club on a Tuesday', 'Thursday Night Code', 'Saturday Night Fever'];
     const sortedEvents = sortByDay(events);
