b8d077e50ce5b046bc0456df3d8d20b7fdb624c9
Scott Falbo <scottfalboart@gmail.com>
2021-01-12 22:32:17 -0800

    wrote InsertShiftArray method, HappyPath and edge case tests pass.

--- c-sharp/challenges/ArrayShift/Program.cs
+++ c-sharp/challenges/ArrayShift/Program.cs
@@ -10,7 +10,27 @@ namespace ArrayShift
         }
         public static int[] InsertShiftArray(int[] array, int value)
         {
-            return array;
+            if (array.Length < 2)
+            {
+                Console.WriteLine("Array must have more than 1 index.");
+                return array;
+            }
+            else
+            {
+                int arrLen = array.Length;
+                int[] returnArr = new int[arrLen + 1];
+                int middleIndex = (arrLen + 1) / 2;
+
+                returnArr[middleIndex] = value;
+                for (int n = 0; n < returnArr.Length; n++)
+                {
+                    if (n < middleIndex)
+                        returnArr[n] = array[n];
+                    if (n > middleIndex)
+                        returnArr[n] = array[n - 1];
+                }
+                return returnArr;
+            }
         }
     }
 }
