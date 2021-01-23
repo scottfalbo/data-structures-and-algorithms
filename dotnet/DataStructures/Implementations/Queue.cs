e971f59f6b24dcbb7823d17ef9dbf7f4d0d1d75e
Scott Falbo <scottfalboart@gmail.com>
2021-01-22 16:12:57 -0800

    added methods to stacks and queues

--- dotnet/DataStructures/Implementations/Queue.cs
+++ dotnet/DataStructures/Implementations/Queue.cs
@@ -2,10 +2,47 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Implementations
+namespace DataStructures
 {
-    class Queue
+    class Queue<T>
     {
+        public Node<T> Front { get; set; }
 
+        public Node<T> Rear { get; set; }
+
+        /// <summary>
+        /// Add a Node object to the Queue object and attatch to to the end of the line
+        /// </summary>
+        /// <param name="value"></param>
+        public void Enqueue(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Returns the Value of the First object in the Queue object and then removes it
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+            Node<T> holder = Front;
+            Front = Front.Next;
+            holder.Next = null;
+            return holder.Value;
+        }
+
+        /// <summary>
+        /// Returns the Value of the Front Node in the Queue object
+        /// </summary>
+        /// <returns></returns>
+        public T Peek() => Front.Value;
+
+        /// <summary>
+        /// Returns true if the Queue object is empty
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty() => Front == null;
     }
 }
