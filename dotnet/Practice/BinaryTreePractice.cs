38b0835258ce709feef75df83b1fb0c437826d66
Scott Falbo <scottfalboart@gmail.com>
2021-11-08 23:29:56 -0800

    Added a class and test class for binary tree practice.  Write helper methos in test to build tree.

---
+++ dotnet/Practice/BinaryTreePractice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures.Trees;
+
+namespace Practice
+{
+    public class BinaryTreePractice
+    {
+        public int FindValue(BinaryTree<int> tree)
+        {
+            int total = 0;
+
+
+
+            return total;
+        }
+    }
+}
