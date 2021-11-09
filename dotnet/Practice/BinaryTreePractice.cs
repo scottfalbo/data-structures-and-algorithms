3db547f4980a59c63aaadf35f07dc814f13cfa22
Scott Falbo <scottfalboart@gmail.com>
2021-11-08 23:46:07 -0800

    Write FineValue() method to find the total value of all nodes in a tree.  Passing test.

--- dotnet/Practice/BinaryTreePractice.cs
+++ dotnet/Practice/BinaryTreePractice.cs
@@ -1,18 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using DataStructures.Trees;
+using DataStructures;
 
 namespace Practice
 {
     public class BinaryTreePractice
     {
+        /// <summary>
+        /// Find the total value of all node values in a BinaryTree<int>
+        /// </summary>
+        /// <param name="tree"> BinaryTree<int> object </param>
+        /// <returns> int total value </returns>
         public static int FindValue(BinaryTree<int> tree)
         {
             int total = 0;
 
+            Queue<DataStructures.Trees.Node<int>> queue = new Queue<DataStructures.Trees.Node<int>>();
+            queue.Enqueue(tree.Root);
 
-
+            while (queue.Front != null)
+            {
+                DataStructures.Trees.Node<int> current = queue.Dequeue();
+                total += current.Value;
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
             return total;
         }
     }
